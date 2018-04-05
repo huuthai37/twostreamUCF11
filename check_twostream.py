ebaa49dc3e698b7ddfa8eed63389f4261d56c75c
Thai Huu <huuthai37@gmail.com>
2018-04-05 23:41:36 +0700

    Add check twostream

---
+++ check_twostream.py
@@ -0,0 +1,38 @@
+import numpy as np 
+import pickle
+import sys
+import cv2
+import config
+
+# check_data.py train opt2
+
+train = sys.argv[1]
+file = sys.argv[2]
+type_data = sys.argv[3]
+server = config.server()
+
+if server:
+	dirr = '/home/oanhnt/thainh/data/database/'
+	dir_data = '/home/oanhnt/thainh/data/rgb/{}/'.format(train)
+else:
+	dirr = '/mnt/data11/database/'
+	dir_data = '/mnt/data11/{}/{}/'.format(file, train)
+
+with open(dirr + train + '-' + file + '.pickle','rb') as f1:
+    data = pickle.load(f1)
+length = len(data)
+print length
+for i in range(length):
+	fileimg = data[i][0]
+	start_opt = data[i][1]
+
+	start_rgb = (int(np.floor(start_opt * opt_size / 20)) + 1 ) * 10
+	rgb = cv2.imread(dir_data + fileimg + '-' + str(start_rgb) + '.jpg')
+
+	if (rgb is None):
+		print data[i]
+		print('Start', start_rgb)
+
+	if i%1000 == 0:
+		print 'Checked {}/{}'.format(i, length)
+
