545e09145348b8b0fa7dd1588c0f13eb1dd65f36
Thai Huu <huuthai37@gmail.com>
2018-04-06 12:36:56 +0700

    Add

---
+++ data_rgb_fix.py
@@ -0,0 +1,110 @@
+import cv2
+import os
+import sys
+import random
+import numpy as np
+import config
+
+# data_rgb.py train 10 run 
+
+def crop_image(frame, name_video, i, y, x):
+    crop = frame[y:y+224, x:x+224].copy()
+    cv2.imwrite(r'{}-{}-{}{}.jpg'.format(name_video, i, y, x), crop)
+    crop_flip = cv2.flip(crop, 1)
+    cv2.imwrite(r'{}-{}-{}{}-flh.jpg'.format(name_video, i, y, x), crop_flip)
+
+# Lay tuy chon 
+train = sys.argv[1]
+sample_rate = int(sys.argv[2])
+if sys.argv[3] == 'run':
+    debug = False
+else:
+    debug = True
+
+# Cau hinh folder du lieu
+server = config.server()
+if server:
+    data_output_folder = r'/home/oanhnt/thainh/data/rgb/{}/'.format(train)
+    data_input_folder = '/home/oanhnt/thainh/UCF-11/'
+    out_file = r'/home/oanhnt/thainh/data/database/{}-rgb.pickle'.format(train)
+else:
+    data_output_folder = r'/mnt/data-11new/rgb/{}/'.format(train)
+    data_input_folder = '/mnt/UCF-11/'
+    out_file = r'/mnt/data-11new/database/{}-rgb.pickle'.format(train)
+
+text_file = r'data/{}list.txt'.format(train)
+class_file = r'data/classInd.txt'
+count = 0
+
+data=[]
+classInd=[]
+
+# Tao class index tu file classInd.txt
+with open(class_file) as f0:
+    for line in f0:
+        class_name = line.rstrip()
+        if class_name:
+            classInd.append(class_name)
+
+
+with open(text_file) as f:
+    for line in f:
+        # Tao duong dan va ten file anh
+        if train != 'test':
+            arr_line = line.split(' ')[0] # return folder/subfolder/name.mpg
+        else:
+            arr_line = line.rstrip()
+        path_video = arr_line.split('/') # return array (folder,subfolder,name.mpg)
+        num_name = len(path_video) # return 3
+        name_video = path_video[num_name - 1].split('.')[0] # return name ko co .mpg
+        folder_video = path_video[0] # return folder
+        path = data_output_folder + folder_video + '/' #return data-output/folder/
+        video_class = classInd.index(folder_video) # index cua video
+
+        # tao folder moi neu chua ton tai
+        if not os.path.isdir(path):
+            os.makedirs(path)
+            print 'Created folder: {}'.format(folder_video)
+
+        cap = cv2.VideoCapture(data_input_folder + arr_line)
+        i = -1
+        os.chdir(path)
+        while(True):
+            # Capture frame-by-frame
+            ret, frame = cap.read()
+            if not ret:
+                break
+                cap.release()
+                sys.exit()
+            i = i + 1
+            if (i%sample_rate != 0):
+                continue
+            
+            height, width, channel = frame.shape
+
+            #random crop max size (heightxheight)
+            x = random.randint(0, (width - height))
+            crop = frame[:, x:x+height].copy()
+
+            resize_img = cv2.resize(crop, (224, 224))
+
+            if not debug:
+                cv2.imwrite(r'{}-{}.jpg'.format(name_video, i),resize_img)
+            data.append([folder_video + '/' + name_video, i, video_class])
+
+            if not debug:
+                crop_flip = resize_img.copy()
+                crop_flip = cv2.flip(crop_flip, 1)
+                cv2.imwrite(r'{}-{}-flip.jpg'.format(name_video, i),crop_flip)
+            # data.append([folder_video + '/' + name_video + '-' + str(i) + '-flip.jpg', video_class])
+            count += 1
+
+        
+        print name_video
+        # Giai phong capture
+        cap.release()
+print 'Generate RGB: {} samples for {} dataset'.format(count,train)
+
+# Ghi du lieu dia chi ra file
+with open(out_file,'wb') as f2:
+    pickle.dump(data,f2)
---
+++ mobilenet_spatial_fix.py
@@ -0,0 +1,186 @@
+import keras
+import sys
+from keras.models import Model
+from keras.layers import Dense, Conv2D, Activation, Reshape, Flatten, Input, ZeroPadding2D
+import get_data as gd
+from keras import optimizers
+import pickle
+import random
+import numpy as np
+import config
+from sklearn.metrics import classification_report
+
+# train: python mobilenet_spatial_fix.py train 32 1 101
+# test: python mobilenet_spatial_fix.py test 32 1 101
+# retrain: python mobilenet_spatial_fix.py retrain 32 1 101 1
+
+if sys.argv[1] == 'train':
+    train = True
+    retrain = False
+    old_epochs = 0
+elif sys.argv[1] == 'retrain':
+    train = True
+    retrain = True
+    old_epochs = int(sys.argv[5])
+else:
+    train = False
+    retrain = False
+
+batch_size = int(sys.argv[2])
+epochs = int(sys.argv[3])
+classes = int(sys.argv[4])
+
+server = config.server()
+if server:
+    if train:
+        out_file = r'/home/oanhnt/thainh/data/database/train-rgb.pickle'
+    else:
+        out_file = r'/home/oanhnt/thainh/data/database/test-rgb.pickle'
+    valid_file = r'/home/oanhnt/thainh/data/database/valid-rgb.pickle'
+else:
+    if train:
+        out_file = r'/mnt/smalldata/database/train-rgb.pickle'
+    else:
+        out_file = r'/mnt/smalldata/database/test-rgb.pickle'
+
+# MobileNet model
+if train & (not retrain):
+    model = keras.applications.mobilenet.MobileNet(
+        include_top=True,  
+        weights='imagenet',
+        dropout=0.5
+    )
+else:
+    model = keras.applications.mobilenet.MobileNet(
+        include_top=True,  
+        dropout=0.5
+    )
+
+# Modify network some last layer
+x = Flatten()(model.layers[-4].output)
+x = Dense(classes, activation='softmax', name='predictions')(x)
+
+#Then create the corresponding model 
+result_model = Model(inputs=model.input, outputs=x)
+# result_model.summary()
+result_model.compile(loss='categorical_crossentropy',
+              optimizer=optimizers.SGD(lr=0.001, decay=1e-6, momentum=0.9, nesterov=True),
+              metrics=['accuracy'])
+
+if train:
+    if retrain:
+        result_model.load_weights('weights/mobilenet_spatial_{}e.h5'.format(old_epochs))
+
+    with open(out_file,'rb') as f1:
+        keys = pickle.load(f1)
+    len_samples = len(keys)
+
+    if server:
+        with open(valid_file,'rb') as f2:
+            keys_valid = pickle.load(f2)
+        len_valid = len(keys_valid)
+
+    print('-'*40)
+    print('MobileNet RGB stream only: Training')
+    print('-'*40)
+    print 'Number samples: {}'.format(len_samples)
+    if server:
+        print 'Number valid: {}'.format(len_valid)
+    histories = []
+    
+    for e in range(epochs):
+        print('-'*40)
+        print('Epoch', e+1)
+        print('-'*40)
+
+        random.shuffle(keys)
+        if server:
+            history = result_model.fit_generator(
+                gd.getTrainData(
+                    keys,
+                    batch_size,
+                    classes,
+                    1,
+                    'train', 
+                    0), 
+                verbose=1, 
+                max_queue_size=2, 
+                steps_per_epoch=len_samples/batch_size, 
+                epochs=1,
+                validation_data=gd.getTrainData(
+                    keys_valid,
+                    batch_size,
+                    classes,
+                    1,
+                    'valid',
+                    0),
+                validation_steps=len_valid/batch_size
+            )
+            histories.append([
+                history.history['acc'],
+                history.history['val_acc'],
+                history.history['loss'],
+                history.history['val_loss']
+            ])
+        else:
+            history = result_model.fit_generator(
+                gd.getTrainData(
+                    keys,
+                    batch_size,
+                    classes,
+                    1,
+                    'train',
+                    0), 
+                verbose=1, 
+                max_queue_size=2, 
+                steps_per_epoch=3, 
+                epochs=1
+            )
+
+            histories.append([
+                history.history['acc'],
+                history.history['loss']
+            ])
+        result_model.save_weights('weights/mobilenet_spatial_{}e.h5'.format(old_epochs+1+e))
+
+        with open('data/trainHistorySpatial_{}_{}e'.format(old_epochs, epochs), 'wb') as file_pi:
+            pickle.dump(histories, file_pi)
+
+else:
+    result_model.load_weights('weights/mobilenet_spatial_{}e.h5'.format(epochs))
+
+    with open(out_file,'rb') as f2:
+        keys = pickle.load(f2)
+    len_samples = len(keys)
+    print('-'*40)
+    print 'MobileNet RGB stream only: Testing'
+    print('-'*40)
+    print 'Number samples: {}'.format(len_samples)
+
+    # score = result_model.evaluate_generator(
+    #     gd.getTrainData(
+    #         keys,
+    #         batch_size,
+    #         classes,
+    #         2,
+    #         'test',
+    #         0), 
+    #     max_queue_size=3, 
+    #     steps=len_samples/batch_size
+    # )
+    # print('Test loss:', score[0])
+    # print('Test accuracy:', score[1])
+
+    Y_test = gd.getClassData(keys)
+    y_pred = result_model.predict_generator(
+        gd.getTrainData(
+            keys,
+            batch_size,
+            classes,
+            1,
+            'test', 
+            0), 
+        max_queue_size=3, 
+        steps=int(np.ceil(len_samples*1.0/batch_size)))
+    y_classes = y_pred.argmax(axis=-1)
+    print(classification_report(Y_test, y_classes, digits=6))
