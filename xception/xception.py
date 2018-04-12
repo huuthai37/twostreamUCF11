b72fee7fbc7f61b98e39bbd6e07fd1e41d68dff9
Thai Huu <huuthai37@gmail.com>
2018-04-12 11:39:03 +0700

    Change

--- xception/xception.py
+++ xception/xception.py
@@ -1,28 +1,7 @@
 # -*- coding: utf-8 -*-
 """Xception V1 model for Keras.
 
-On ImageNet, this model gets to a top-1 validation accuracy of 0.790
-and a top-5 validation accuracy of 0.945.
-
-Do note that the input image format for this model is different than for
-the VGG16 and ResNet models (299x299 instead of 224x224),
-and that the input preprocessing function
-is also different (same as Inception V3).
-
-Also do note that this model is only available for the TensorFlow backend,
-due to its reliance on `SeparableConvolution` layers.
-
-# Reference
-
-- [Xception: Deep Learning with Depthwise Separable Convolutions](https://arxiv.org/abs/1610.02357)
-
 """
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-import os
-import warnings
 
 from keras.models import Model
 from keras import layers
@@ -35,229 +14,13 @@ from keras.layers import SeparableConv2D
 from keras.layers import MaxPooling2D
 from keras.layers import GlobalAveragePooling2D
 from keras.layers import GlobalMaxPooling2D
-from keras.engine.topology import get_source_inputs
-from keras.utils.data_utils import get_file
-from keras import backend as K
-from keras import applications
-from keras.applications import imagenet_utils
-from keras.applications.imagenet_utils import decode_predictions
-from keras.applications.imagenet_utils import _obtain_input_shape
-import numpy as np
 
+def XceptionFix(include_top=True, input_shape=None, classes=1000, weights=None):
 
-TF_WEIGHTS_PATH = 'https://github.com/fchollet/deep-learning-models/releases/download/v0.4/xception_weights_tf_dim_ordering_tf_kernels.h5'
-TF_WEIGHTS_PATH_NO_TOP = 'https://github.com/fchollet/deep-learning-models/releases/download/v0.4/xception_weights_tf_dim_ordering_tf_kernels_notop.h5'
-
-
-def XceptionFix(include_top=True, weights='imagenet',
-             input_tensor=None, input_shape=None,
-             pooling=None,
-             classes=1000):
-
-    if K.backend() != 'tensorflow':
-        raise RuntimeError('The Xception model is only available with '
-                           'the TensorFlow backend.')
-    if K.image_data_format() != 'channels_last':
-        warnings.warn('The Xception model is only available for the '
-                      'input data format "channels_last" '
-                      '(width, height, channels). '
-                      'However your settings specify the default '
-                      'data format "channels_first" (channels, width, height). '
-                      'You should set `image_data_format="channels_last"` in your Keras '
-                      'config located at ~/.keras/keras.json. '
-                      'The model being returned right now will expect inputs '
-                      'to follow the "channels_last" data format.')
-        K.set_image_data_format('channels_last')
-        old_data_format = 'channels_first'
+    if input_shape is None:
+        img_input = Input((299,299,3))
     else:
-        old_data_format = None
-
-    img_input = Input(shape=input_shape)
-
-    x = Conv2D(32, (3, 3), strides=(2, 2), use_bias=False, name='block1_conv1_new')(img_input)
-    x = BatchNormalization(name='block1_conv1_bn')(x)
-    x = Activation('relu', name='block1_conv1_act')(x)
-    x = Conv2D(64, (3, 3), use_bias=False, name='block1_conv2')(x)
-    x = BatchNormalization(name='block1_conv2_bn')(x)
-    x = Activation('relu', name='block1_conv2_act')(x)
-
-    residual = Conv2D(128, (1, 1), strides=(2, 2),
-                      padding='same', use_bias=False)(x)
-    residual = BatchNormalization()(residual)
-
-    x = SeparableConv2D(128, (3, 3), padding='same', use_bias=False, name='block2_sepconv1')(x)
-    x = BatchNormalization(name='block2_sepconv1_bn')(x)
-    x = Activation('relu', name='block2_sepconv2_act')(x)
-    x = SeparableConv2D(128, (3, 3), padding='same', use_bias=False, name='block2_sepconv2')(x)
-    x = BatchNormalization(name='block2_sepconv2_bn')(x)
-
-    x = MaxPooling2D((3, 3), strides=(2, 2), padding='same', name='block2_pool')(x)
-    x = layers.add([x, residual])
-
-    residual = Conv2D(256, (1, 1), strides=(2, 2),
-                      padding='same', use_bias=False)(x)
-    residual = BatchNormalization()(residual)
-
-    x = Activation('relu', name='block3_sepconv1_act')(x)
-    x = SeparableConv2D(256, (3, 3), padding='same', use_bias=False, name='block3_sepconv1')(x)
-    x = BatchNormalization(name='block3_sepconv1_bn')(x)
-    x = Activation('relu', name='block3_sepconv2_act')(x)
-    x = SeparableConv2D(256, (3, 3), padding='same', use_bias=False, name='block3_sepconv2')(x)
-    x = BatchNormalization(name='block3_sepconv2_bn')(x)
-
-    x = MaxPooling2D((3, 3), strides=(2, 2), padding='same', name='block3_pool')(x)
-    x = layers.add([x, residual])
-
-    residual = Conv2D(728, (1, 1), strides=(2, 2),
-                      padding='same', use_bias=False)(x)
-    residual = BatchNormalization()(residual)
-
-    x = Activation('relu', name='block4_sepconv1_act')(x)
-    x = SeparableConv2D(728, (3, 3), padding='same', use_bias=False, name='block4_sepconv1')(x)
-    x = BatchNormalization(name='block4_sepconv1_bn')(x)
-    x = Activation('relu', name='block4_sepconv2_act')(x)
-    x = SeparableConv2D(728, (3, 3), padding='same', use_bias=False, name='block4_sepconv2')(x)
-    x = BatchNormalization(name='block4_sepconv2_bn')(x)
-
-    x = MaxPooling2D((3, 3), strides=(2, 2), padding='same', name='block4_pool')(x)
-    x = layers.add([x, residual])
-
-    for i in range(8):
-        residual = x
-        prefix = 'block' + str(i + 5)
-
-        x = Activation('relu', name=prefix + '_sepconv1_act')(x)
-        x = SeparableConv2D(728, (3, 3), padding='same', use_bias=False, name=prefix + '_sepconv1')(x)
-        x = BatchNormalization(name=prefix + '_sepconv1_bn')(x)
-        x = Activation('relu', name=prefix + '_sepconv2_act')(x)
-        x = SeparableConv2D(728, (3, 3), padding='same', use_bias=False, name=prefix + '_sepconv2')(x)
-        x = BatchNormalization(name=prefix + '_sepconv2_bn')(x)
-        x = Activation('relu', name=prefix + '_sepconv3_act')(x)
-        x = SeparableConv2D(728, (3, 3), padding='same', use_bias=False, name=prefix + '_sepconv3')(x)
-        x = BatchNormalization(name=prefix + '_sepconv3_bn')(x)
-
-        x = layers.add([x, residual])
-
-    residual = Conv2D(1024, (1, 1), strides=(2, 2),
-                      padding='same', use_bias=False)(x)
-    residual = BatchNormalization()(residual)
-
-    x = Activation('relu', name='block13_sepconv1_act')(x)
-    x = SeparableConv2D(728, (3, 3), padding='same', use_bias=False, name='block13_sepconv1')(x)
-    x = BatchNormalization(name='block13_sepconv1_bn')(x)
-    x = Activation('relu', name='block13_sepconv2_act')(x)
-    x = SeparableConv2D(1024, (3, 3), padding='same', use_bias=False, name='block13_sepconv2')(x)
-    x = BatchNormalization(name='block13_sepconv2_bn')(x)
-
-    x = MaxPooling2D((3, 3), strides=(2, 2), padding='same', name='block13_pool')(x)
-    x = layers.add([x, residual])
-
-    x = SeparableConv2D(1536, (3, 3), padding='same', use_bias=False, name='block14_sepconv1')(x)
-    x = BatchNormalization(name='block14_sepconv1_bn')(x)
-    x = Activation('relu', name='block14_sepconv1_act')(x)
-
-    x = SeparableConv2D(2048, (3, 3), padding='same', use_bias=False, name='block14_sepconv2')(x)
-    x = BatchNormalization(name='block14_sepconv2_bn')(x)
-    x = Activation('relu', name='block14_sepconv2_act')(x)
-
-    if include_top:
-        x = GlobalAveragePooling2D(name='avg_pool')(x)
-        x = Dense(1000, activation='softmax', name='predictions')(x)
-    else:
-        if pooling == 'avg':
-            x = GlobalAveragePooling2D()(x)
-        elif pooling == 'max':
-            x = GlobalMaxPooling2D()(x)
-
-    # Ensure that the model takes into account
-    # any potential predecessors of `input_tensor`.
-    if input_tensor is not None:
-        inputs = get_source_inputs(input_tensor)
-    else:
-        inputs = img_input
-    # Create model.
-    model = Model(inputs, x, name='xception')
-
-    # load weights
-    if weights == 'imagenet':
-        if include_top:
-            weights_path = get_file('xception_weights_tf_dim_ordering_tf_kernels.h5',
-                                    TF_WEIGHTS_PATH,
-                                    cache_subdir='models',
-                                    file_hash='0a58e3b7378bc2990ea3b43d5981f1f6')
-        else:
-            weights_path = get_file('xception_weights_tf_dim_ordering_tf_kernels_notop.h5',
-                                    TF_WEIGHTS_PATH_NO_TOP,
-                                    cache_subdir='models',
-                                    file_hash='b0042744bf5b25fce3cb969f33bebb97')
-        model.load_weights(weights_path, by_name=True)
-    elif weights is not None:
-        model.load_weights(weights)
-
-    z = Dense(classes, activation='softmax')(model.layers[-2].output)
-    new_model = Model(model.input, z)
-
-    sample_model = applications.xception.Xception(
-        include_top=True,  
-        weights='imagenet', 
-    )
-
-    new_weight = convert_weights(sample_model.get_layer('block1_conv1').get_weights(), 20)
-
-    new_model.get_layer('block1_conv1_new').set_weights(new_weight)
-
-    if old_data_format:
-        K.set_image_data_format(old_data_format)
-    return new_model
-
-def XceptionFixNoName(include_top=True, weights=None,
-             input_tensor=None, input_shape=None,
-             pooling=None,pre='',
-             classes=1000):
-
-    if not (weights in {'imagenet', None} or os.path.exists(weights)):
-        raise ValueError('The `weights` argument should be either '
-                         '`None` (random initialization), `imagenet` '
-                         '(pre-training on ImageNet), '
-                         'or the path to the weights file to be loaded.')
-
-    # if weights == 'imagenet' and include_top and classes != 1000:
-    #   raise ValueError('If using `weights` as imagenet with `include_top`'
-    #                    ' as true, `classes` should be 1000')
-
-    if K.backend() != 'tensorflow':
-        raise RuntimeError('The Xception model is only available with '
-                           'the TensorFlow backend.')
-    if K.image_data_format() != 'channels_last':
-        warnings.warn('The Xception model is only available for the '
-                      'input data format "channels_last" '
-                      '(width, height, channels). '
-                      'However your settings specify the default '
-                      'data format "channels_first" (channels, width, height). '
-                      'You should set `image_data_format="channels_last"` in your Keras '
-                      'config located at ~/.keras/keras.json. '
-                      'The model being returned right now will expect inputs '
-                      'to follow the "channels_last" data format.')
-        K.set_image_data_format('channels_last')
-        old_data_format = 'channels_first'
-    else:
-        old_data_format = None
-
-    # Determine proper input shape
-    # input_shape = _obtain_input_shape(input_shape,
-    #                                 default_size=299,
-    #                                 min_size=71,
-    #                                 data_format=K.image_data_format(),
-    #                                 require_flatten=False,
-    #                                 weights=weights)
-
-    if input_tensor is None:
-        img_input = Input(shape=(299,299,3))
-    else:
-        if not K.is_keras_tensor(input_tensor):
-            img_input = Input(tensor=input_tensor, shape=(299,299,3))
-        else:
-            img_input = input_tensor
+        img_input = Input(input_shape)
 
     x = Conv2D(32, (3, 3), strides=(2, 2), use_bias=False)(img_input)
     x = BatchNormalization()(x)
@@ -309,17 +72,17 @@ def XceptionFixNoName(include_top=True, weights=None,
 
     for i in range(8):
         residual = x
-        prefix = pre + 'block' + str(i + 5)
+        prefix = 'block' + str(i + 5)
 
-        x = Activation('relu', name=prefix + '_sepconv1_act')(x)
-        x = SeparableConv2D(728, (3, 3), padding='same', use_bias=False, name=prefix + '_sepconv1')(x)
-        x = BatchNormalization(name=prefix + '_sepconv1_bn')(x)
-        x = Activation('relu', name=prefix + '_sepconv2_act')(x)
-        x = SeparableConv2D(728, (3, 3), padding='same', use_bias=False, name=prefix + '_sepconv2')(x)
-        x = BatchNormalization(name=prefix + '_sepconv2_bn')(x)
-        x = Activation('relu', name=prefix + '_sepconv3_act')(x)
-        x = SeparableConv2D(728, (3, 3), padding='same', use_bias=False, name=prefix + '_sepconv3')(x)
-        x = BatchNormalization(name=prefix + '_sepconv3_bn')(x)
+        x = Activation('relu')(x)
+        x = SeparableConv2D(728, (3, 3), padding='same', use_bias=False)(x)
+        x = BatchNormalization()(x)
+        x = Activation('relu')(x)
+        x = SeparableConv2D(728, (3, 3), padding='same', use_bias=False)(x)
+        x = BatchNormalization()(x)
+        x = Activation('relu')(x)
+        x = SeparableConv2D(728, (3, 3), padding='same', use_bias=False)(x)
+        x = BatchNormalization()(x)
 
         x = layers.add([x, residual])
 
@@ -348,26 +111,9 @@ def XceptionFixNoName(include_top=True, weights=None,
     if include_top:
         x = GlobalAveragePooling2D()(x)
         x = Dense(classes, activation='softmax')(x)
+    
+    model = Model(img_input, x)
 
-    # Ensure that the model takes into account
-    # any potential predecessors of `input_tensor`.
-    if input_tensor is not None:
-        inputs = get_source_inputs(input_tensor)
-    else:
-        inputs = img_input
-    # Create model.
-    model = Model(inputs, x)
-
-    if old_data_format:
-        K.set_image_data_format(old_data_format)
-
+    if weights is not None:
+        model.load_weights(weights)
     return model
-
-def convert_weights(weights, depth):
-    mat = weights[0]
-    mat2 = np.empty([3,3,depth,32])
-    for i in range(32):
-        x=(mat[:,:,0,i] + mat[:,:,1,i] + mat[:,:,2,i])/3
-        for j in range(depth):
-            mat2[:,:,j,i] = x
-    return [mat2]
