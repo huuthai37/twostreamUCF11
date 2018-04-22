339993bc9164ffea762c2e56335edd82f43e5681
Thai Huu <huuthai37@gmail.com>
2018-04-23 01:52:01 +0700

    change

--- data_all.py
+++ data_all.py
@@ -4,6 +4,7 @@ import sys
 import config
 
 train = sys.argv[1]
+debug = sys.argv[2]
 server = config.server()
 
 if server:
@@ -16,9 +17,11 @@ with open(opt_file,'rb') as f1:
 l = len(opt)
 opt_all = []
 for i in range(l):
-	opt_all.append([opt[i][0], opt[i][1], opt[i][2], (opt[i][1]*2), (opt[i][1]*4)])
-	opt_all.append([opt[i][0], opt[i][1], opt[i][2], (opt[i][1]*2 + 10), ((opt[i][1] * 2 + 10) * 2)])
+	if opt[i][1] % 20 == 0:
+		opt_all.append([opt[i][0], opt[i][1], opt[i][2], (opt[i][1]*2), (opt[i][1]*4)])
+		opt_all.append([opt[i][0], opt[i][1], opt[i][2], (opt[i][1]*2 + 10), ((opt[i][1] * 2 + 10) * 2)])
 
 print len(opt_all)
-with open(out_file,'wb') as f3:
-    pickle.dump(opt_all,f3)
+if debug == 'run':
+	with open(out_file,'wb') as f3:
+	    pickle.dump(opt_all,f3)
