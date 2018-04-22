384edf482c7b734f68c1d6a529cb18007c2d7892
Thai Huu <huuthai37@gmail.com>
2018-04-23 01:32:31 +0700

    change

--- data_all.py
+++ data_all.py
@@ -16,8 +16,8 @@ with open(opt_file,'rb') as f1:
 l = len(opt)
 opt_all = []
 for i in range(l):
-	opt_all.append([opt[0], opt[1], opt[2], opt[1]*2, opt[1]*4])
-	opt_all.append([opt[0], opt[1], opt[2], opt[1]*2 + 10, (opt[1] * 2 + 10) * 2])
+	opt_all.append([opt[i][0], opt[i][1], opt[i][2], (opt[i][1]*2), (opt[i][1]*4)])
+	opt_all.append([opt[i][0], opt[i][1], opt[i][2], (opt[i][1]*2 + 10), ((opt[i][1] * 2 + 10) * 2)])
 
 print len(opt_all)
 with open(out_file,'wb') as f3:
